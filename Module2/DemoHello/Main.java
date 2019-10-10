9caf9e74e5509c1fcb1c0edc833d2b8bbfd0a758
trungnam95 <nguyentrungnam100495@gmail.com>
2019-10-10 11:09:20 +0700

    work

---
+++ Module2/DemoHello/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "2";
+        int a =0;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+            System.out.println("newbie");
+        }
+    }
+}
